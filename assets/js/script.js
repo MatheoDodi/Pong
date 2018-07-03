07b7779738002c293fb933d11f68e207c7a774a1
Matthew Dodi <matthew.dodi@gmail.com>
2018-07-02 23:24:44 -0700

    Added enemy paddle. Enemy paddle should be able to chase the ball according to it's position

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
 let c = document.getElementById("myCanvas");
 let ctx = c.getContext("2d");
 let ballX = 700; //starting position of ball on X-axis
-let ballY = 300; //starting position of ball on Y-axis
+let ballY = 400; //starting position of ball on Y-axis
 let ballSpeedX = 5; //speed of ball on X-axis
-let ballSpeedY = 3; // speed of ball on Y-axis
+let ballSpeedY = 3.2; // speed of ball on Y-axis
 const FPS = 1000 / 60; //frames per second
 const CANVAS_WIDTH = c.width; //total width of the canvas
 const CANVAS_HEIGHT = c.height; //total height of the canvas
 let p1Paddle = 400;
 let p1Speed = 15;
+let p2Paddle = 450;
+let p2Speed = 3.154;
+
+function youWin() {
+	window.alert(youWin);
+}
 
 function moveP1Paddle(event) {
 	if (event.keyCode === 40) { //checks to see if the DOWN ARROW was pressed
@@ -27,28 +33,42 @@ function moveP1Paddle(event) {
 function startGame() {
 	ballMovement(); 
 	drawCanvasAndBall();
+
 }
 
 function checkBallCollision() {
 
 	if (ballX + 10 > CANVAS_WIDTH) {
-		ballSpeedX = -ballSpeedX; //if the ball goes out of frame on the right side, changes it's direction by turning the "5" -> "-5"
+		ballSpeedX = -ballSpeedX; //if the ball hits the right border of the canvas, it reverses it's direction turning the "5" -> "-5"
 	} else if (ballX < 0) {
-		ballSpeedX = -ballSpeedX; //if the ball goes out of frame on the left side, changes it's direction by turning the "-5" -> "5"
+		ballSpeedX = -ballSpeedX; //if the ball hits the left border of the canvas, it reverses it's direction turning the "-5" -> "5"
 	} else if (ballY + 10 > CANVAS_HEIGHT) {
-		ballSpeedY = -ballSpeedY; //if the ball goes out of frame on the top side, changes it's direction by turning the "3" -> "-3"
+		ballSpeedY = -ballSpeedY; //if the ball goes out of frame on the top side, changes it's direction by turning the "3.2" -> "-3.2"
 	} else if (ballY < 0) {
-		ballSpeedY = -ballSpeedY; //if the ball goes out of frame on the top side, changes it's direction by turning the "-3" -> "3"
-	} else if ( ballX === 60 && ballY >= p1Paddle && ballY <= p1Paddle + 80) {
-		ballSpeedX = -ballSpeedX
+		ballSpeedY = -ballSpeedY; //if the ball goes out of frame on the top side, changes it's direction by turning the "-3.2" -> "3.2"
+	} else if ( ballX === 60 && ballY >= p1Paddle && ballY <= p1Paddle + 80) {  
+		ballSpeedX = -ballSpeedX;
+	} else if (ballX === 940 && ballY >= p2Paddle && ballY <= p2Paddle + 80) {
+		ballSpeedX = -ballSpeedX;
 	}
+}
 
+
+
+
+function chaseBall() {
+	if (ballY > p2Paddle + 70) {
+		p2Paddle += p2Speed;
+	} else if (ballY < p2Paddle + 10) {
+		p2Paddle -= p2Speed;
+	}
 }
 
 function ballMovement() {
 	ballX += ballSpeedX; //movement of the ball on X-axis
 	ballY += ballSpeedY; //movement of the ball on Y-axis
 }
+
 
 function drawCanvasAndBall() {
 	//next lines draw the black canvas
@@ -65,7 +85,13 @@ function drawCanvasAndBall() {
 	ctx.fillStyle = 'white';
 	ctx.fillRect(50,p1Paddle,10,80);
 
-	checkBallCollision() //calls the function to check if the ball is out of bounds or has collided with the paddles every frame
+	//next lines draw the right paddle
+	ctx.fillStyle = 'white';
+	ctx.fillRect(940,p2Paddle,10,80)
+
+	checkBallCollision()
+	chaseBall()
+
 	console.log(p1Paddle);
 	console.log(ballX, ballY)
 
